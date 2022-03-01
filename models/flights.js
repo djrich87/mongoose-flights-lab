1707f09f3a71bacb45375a272c9f8cbac0a2028a
djrich87 <djrich87@gmail.com>
2022-03-01 15:40:55 -0700

    commit

--- models/flights.js
+++ models/flights.js
@@ -2,3 +2,24 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
+const flightsSchema = new Schema({
+  airline: {
+    type: String,
+    enum: ['American', 'Southwest', 'United']
+  },
+
+  airports: {
+    type: String,
+    enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'],
+    default: "DEN"
+
+  },
+
+  flightNo: {
+
+
+  }
+
+
+
+})
