7c28a4a75605c8d343bff16eed1d5fafc456173d
djrich87 <djrich87@gmail.com>
2022-03-01 15:51:51 -0700

    commit

--- models/flights.js
+++ models/flights.js
@@ -12,14 +12,17 @@ const flightsSchema = new Schema({
     type: String,
     enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'],
     default: "DEN"
-
   },
 
   flightNo: {
+    type: Number,
+    min: 10,
+    max: 9999,
+  },
 
-
+  departs: {
+    type: Date,
+    default: Date.now() +
   }
-
-
 
 })
